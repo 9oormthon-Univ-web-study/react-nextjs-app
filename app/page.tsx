b4fbdd12a064a9b10994971301db162f34c7a587
kojesung <js95112345@gmail.com>
2024-09-23 20:31:04 +0900

    fix : 폴더 구조 수정(app 폴더 라우팅으로 변경)

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
-import Head from 'next/head';
-import homestyles from '../styles/Home.module.css';
-import { GetStaticProps } from 'next';
+import { Metadata } from 'next';
+import homestyles from '../src/styles/Home.module.css';
 import { getSortedPostsData } from '@/lib/post';
 import Link from 'next/link';
 
-export default function Home({ allPostData }: { allPostData: { date: string; title: string; id: string }[] }) {
+export const metadata: Metadata = {
+    title: '고제성',
+};
+
+export default async function Home() {
+    const allPostData = await getSortedPostsData();
+
     return (
         <>
-            <Head>
-                {/* 탭 이름 바꿔주는 부분 */}
-                <title>고제성</title>
-            </Head>
             <section className={homestyles.headingMd}>
                 <p>[Your Self Introduction]</p>
                 <p>(This is a website)</p>
@@ -22,7 +23,6 @@ export default function Home({ allPostData }: { allPostData: { date: string; tit
                         <li className={homestyles.listItem} key={id}>
                             <Link href={`posts/${id}`}>
                                 <p>{title}</p>
-                                {/* 원래 <a>였는데 <Link>랑 같이 쓸 수 없다해서 수정 */}
                             </Link>
                             <br />
                             <small className={homestyles.lightText}>{date}</small>
@@ -34,12 +34,3 @@ export default function Home({ allPostData }: { allPostData: { date: string; tit
         </>
     );
 }
-
-export const getStaticProps: GetStaticProps = async () => {
-    const allPostData = getSortedPostsData(); // md파일을 날짜별로 정렬해서 반환해줌
-    return {
-        props: {
-            allPostData,
-        },
-    };
-};
