9d2cddf4943be850e85b3c30d100ecfe02eb606b
kojesung <js95112345@gmail.com>
2024-09-23 22:03:08 +0900

    fix : type any -> Post

--- src/app/posts2/page.tsx
+++ src/app/posts2/page.tsx
@@ -1,14 +1,24 @@
 import Link from 'next/link';
 import React from 'react';
 
+interface Post {
+    collectionId: string;
+    collectionName: string;
+    created: string;
+    id: string;
+    title: string;
+    updated: string;
+}
+
 const getPost = async () => {
     const res = await fetch('http://127.0.0.1:8090/api/collections/posts/records');
     const data = await res.json();
-    return data?.items as any[];
+    return data?.items as Post[];
 };
 
 const PostPage = async () => {
     const posts = await getPost();
+    console.log(posts);
     return (
         <div>
             <h1>Posts</h1>
@@ -21,7 +31,7 @@ const PostPage = async () => {
 
 export default PostPage;
 
-const PostItem = ({ post }: any) => {
+const PostItem = ({ post }: { post: Post }) => {
     const { id, title, created } = post || {};
     return (
         <Link href={`posts/${id}`}>
