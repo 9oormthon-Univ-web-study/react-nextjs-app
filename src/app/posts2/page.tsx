d859bfbe50a950ea6d77f1dd48b37dac7da92933
kojesung <js95112345@gmail.com>
2024-09-24 16:03:27 +0900

    feat : App route test code

--- src/app/posts2/page.tsx
+++ src/app/posts2/page.tsx
@@ -11,14 +11,16 @@ interface Post {
 }
 
 const getPost = async () => {
-    const res = await fetch('http://127.0.0.1:8090/api/collections/posts/records');
+    const res = await fetch(
+        'http://127.0.0.1:8090/api/collections/posts/records',
+        { cache: 'no-store' } /* 캐시 저장 안하도록 */
+    );
     const data = await res.json();
     return data?.items as Post[];
 };
 
 const PostPage = async () => {
     const posts = await getPost();
-    console.log(posts);
     return (
         <div>
             <h1>Posts</h1>
@@ -34,7 +36,7 @@ export default PostPage;
 const PostItem = ({ post }: { post: Post }) => {
     const { id, title, created } = post || {};
     return (
-        <Link href={`posts/${id}`}>
+        <Link href={`posts2/${id}`}>
             <div>
                 <h3>{title}</h3>
                 <p>{created}</p>
